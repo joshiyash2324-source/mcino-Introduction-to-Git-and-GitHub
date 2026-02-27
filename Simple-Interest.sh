26d9f6e27ff6337631de242894256dc534eb7945
Yashvardhan Joshi <joshiyash2324@gmail.com>
2026-02-27 20:27:41 +0530

    Add working Simple-Interest.sh script

--- Simple-Interest.sh
+++ Simple-Interest.sh
@@ -4,8 +4,8 @@
 
 # Read user input
 read -p "Enter Principal amount: " P
-read -p "Enter Rate of Interest (in %): " R
-read -p "Enter Time period (in years): " T
+read -p "Enter Rate of Interest (in %) : " R
+read -p "Enter Time period (in years) : " T
 
 # Calculate Simple Interest
 SI=$((P * R * T / 100))
